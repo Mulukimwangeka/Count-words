812dfac899a62650200c5e3a3a3ac4131f3f477f
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-16 09:06:30 +0300

    count done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String words = "One Two Three Four Five Six Seven Eight Nine Ten" ;
+        int countWords = words.split("\\s").length;
+        System.out.println(countWords);
     }
 }
